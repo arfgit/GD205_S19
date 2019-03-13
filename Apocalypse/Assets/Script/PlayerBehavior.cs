892accb41665543001bd347954142a89efe4872b
Sinzephy <courtneyramos65@gmail.com>
2019-03-13 13:21:27 -0400

    player movement

---
+++ Apocalypse/Assets/Script/PlayerBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal")*Time.deltaTime,0f,moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);
+    }
+}
