321b44b6e0ccfc94cc60ee899a3f06eaef23b9ed
arfgit <47192556+arfgit@users.noreply.github.com>
2019-03-27 13:45:34 -0400

    edit

---
+++ Apocalypse/Assets/Script/FollowCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+   public GameObject target;
+   Vector3 offset;
+    void Start()
+    {
+        offset = transform.position - target.transform.position;
+
+    }
+
+
+    void LateUpdate()
+    {
+        transform.position = target.transform.position + offset;
+
+
+    }
+}
