7b6636975699476c97e95951f9020c12f6472e0b
arfgit <47192556+arfgit@users.noreply.github.com>
2019-04-03 16:48:36 -0400

    edit

---
+++ Apocalypse/Assets/Script/ElvisAnim.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElvisAnim : MonoBehaviour
+{
+    Animator myAnim;
+
+    void Start()
+    {
+
+    myAnim = GetComponent<Animator>();
+
+    }
+
+
+    void Update()
+    {
+        
+        myAnim.SetInteger("state", 0);
+        if(Input.GetKey(KeyCode.W))
+            {
+            myAnim.SetInteger("state", 1);
+
+            }
+            
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myAnim.SetInteger("state", 1);
+
+        }
+    }
+
+    }
+    
+
