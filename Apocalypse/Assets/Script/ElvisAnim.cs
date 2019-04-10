6b310529115d57a2a5b3e67831ae7403d7b5eeba
arfgit <47192556+arfgit@users.noreply.github.com>
2019-04-10 13:26:16 -0400

    elvis has his animations

--- Apocalypse/Assets/Script/ElvisAnim.cs
+++ Apocalypse/Assets/Script/ElvisAnim.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -25,11 +25,26 @@ public class ElvisAnim : MonoBehaviour
             }
          
          myAnim.SetInteger("jump", 0);
-        if (Input.GetKey(KeyCode.Space))
-        {
+        if (Input.GetKeyDown(KeyCode.Space))
+            {
             myAnim.SetInteger("jump", 1);
 
-        }
+            }
+         myAnim.SetInteger("run", 0);
+        if (Input.GetKey(KeyCode.LeftShift))
+            {
+            myAnim.SetInteger("run", 1);
+            }
+              myAnim.SetInteger("rightTurn", 0);
+        if (Input.GetKey(KeyCode.E))
+            {
+            myAnim.SetInteger("rightTurn", 1);
+            }
+                   myAnim.SetInteger("leftTurn", 0);
+        if (Input.GetKeyDown(KeyCode.Q))
+            {
+            myAnim.SetInteger("leftTurn", 1);
+            }
     }
 
     }
