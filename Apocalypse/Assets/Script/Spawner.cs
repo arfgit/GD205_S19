899fdf263773f8c9f27d81767d621bf9494ba1fd
Anthony Feliz <47192556+arfgit@users.noreply.github.com>
2019-04-17 02:53:15 -0400

    edit

---
+++ Apocalypse/Assets/Script/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject clone;
+    public Vector3 spawnLocation;
+
+    void Start()
+    {
+        
+        
+
+    }
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R)){
+            
+            Instantiate(clone, spawnLocation, Quaternion.identity );
+
+
+        }
+
+
+    }
+}
