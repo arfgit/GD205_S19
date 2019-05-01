1b5d9cccf959e0cd8e0b88379340aad5a7a4f628
arfgit <47192556+arfgit@users.noreply.github.com>
2019-05-01 15:50:39 -0400

    W.13

---
+++ W.12/Assets/MyNavMesh.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MyNavMesh : MonoBehaviour
+{
+    NavMeshAgent nav;
+    public Transform target;
+ 
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+
+
+    }
+
+ 
+    void Update()
+    {
+        nav.destination = target.transform.position;
+
+
+    }
+}
