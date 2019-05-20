5ecdae8162f3e890f1ed9cc42c19dd8aecd39ddf
Anthony Feliz <47192556+arfgit@users.noreply.github.com>
2019-05-20 01:42:19 -0400

    game updated
    game idea has been changed

---
+++ Apocalypse/Assets/Script/ScriptsFP/HeadBob.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//credit to this script is given to the headbobber code from the UnifyCommunity. The code was translated from JavaScript to C#.
+public class HeadBob : MonoBehaviour
+{
+//variables
+private float timer = 0.0f;
+float bobbingSpeed = 0.18f;
+float bobbingAmount = 0.2f;
+float midpoint = 1.8f;
+
+
+  
+  void Update () {
+     float waveslice = 0.0f;
+     float horizontal = Input.GetAxis("Horizontal");
+     float vertical = Input.GetAxis("Vertical");
+  
+  Vector3 cSharpConversion = transform.localPosition; 
+  
+     if (Mathf.Abs(horizontal) == 0 && Mathf.Abs(vertical) == 0) {
+        timer = 0.0f;
+     }
+     else {
+        waveslice = Mathf.Sin(timer);
+        timer = timer + bobbingSpeed;
+        if (timer > Mathf.PI * 2) {
+           timer = timer - (Mathf.PI * 2);
+        }
+     }
+     if (waveslice != 0) {
+        float translateChange = waveslice * bobbingAmount;
+        float totalAxes = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
+        totalAxes = Mathf.Clamp (totalAxes, 0.0f, 1.0f);
+        translateChange = totalAxes * translateChange;
+        cSharpConversion.y = midpoint + translateChange;
+     }
+     else {
+        cSharpConversion.y = midpoint;
+     }
+  
+  transform.localPosition = cSharpConversion;
+  }
+  
+  
+
+
+
+}
---
+++ Apocalypse/Assets/Script/ScriptsFP/MovementFP.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//attaches the component placed ot the character
+[RequireComponent(typeof(CharacterController))]
+public class MovementFP : MonoBehaviour
+{
+    // variables //
+     public float playerWalkingSpeed = 5f; //controls player walking speed
+    public float playerRunningSpeed = 10f; //controls players running speed
+    public float jumpStrength = 5f; //controls player 
+    public float verticalRotationLimit = 80f; //limits how high the person can look up and down on the vertical axis
+    
+    float fowardMovement; //foward
+    float sidewaysMovement; //sideways
+
+    float verticalJump; //jump height
+
+    
+    
+    float verticalRotation = 0;
+    CharacterController cc;
+
+    void Awake() //initializes variables beofre the game starts
+    {
+        cc= GetComponent<CharacterController>();
+        Cursor.visible = false; //hides the cursor once i hit play
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    void Update()
+    {
+        //mouse camera movement
+           
+            //horizontal camera movement
+        float horizontalRotation = Input.GetAxis("Mouse X");
+        transform.Rotate(0, horizontalRotation, 0);
+           
+            //vertical camera movement
+                //how high and low the player can look has been limited.
+        if(cc.isGrounded){
+        verticalRotation -= Input.GetAxis("Mouse Y");
+        verticalRotation = Mathf.Clamp(verticalRotation, -verticalRotationLimit, verticalRotationLimit); //(vairable we want to control, first value, second value)
+        Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0 ,0);
+
+        //foward movement for walk
+        fowardMovement = Input.GetAxis("Vertical") * playerWalkingSpeed;
+
+        //sideways movement for walk
+        sidewaysMovement = Input.GetAxis("Horizontal") * playerWalkingSpeed;
+
+        if(Input.GetKey(KeyCode.LeftShift))
+            {
+        //foward movement for run
+        fowardMovement = Input.GetAxis("Vertical") * playerRunningSpeed;
+
+        //sideways movement for run
+        sidewaysMovement = Input.GetAxis("Horizontal") * playerRunningSpeed;
+
+            }
+        }
+        
+
+        verticalJump += Physics.gravity.y * Time.deltaTime;
+
+
+
+          if(Input.GetButton("Jump") && cc.isGrounded)
+        {
+            verticalJump = jumpStrength;
+
+        }
+
+        //x = sideways movement, y = jump, z = foward movement
+        Vector3 playerMovement = new Vector3
+        (sidewaysMovement, verticalJump, fowardMovement);
+
+
+        
+        //moves the controller by motion
+        cc.Move(transform.rotation * playerMovement * Time.deltaTime);
+
+    }
+}
