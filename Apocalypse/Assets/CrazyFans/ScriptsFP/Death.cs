3cc1d4c928a08e00bb4637c65305e623573758db
Anthony Feliz <47192556+arfgit@users.noreply.github.com>
2019-05-22 11:25:18 -0400

    Final edit (hopefully)

---
+++ Apocalypse/Assets/CrazyFans/ScriptsFP/Death.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour
+{
+  void OnTriggerEnter(Collider other){
+      if(other.tag == "Finish")
+      {
+        SceneManager.LoadScene("WinFP");
+
+      }
+    }
+    void OnCollisionEnter(Collision col){
+    if ((col.gameObject.tag == "Bad"))
+        {
+        SceneManager.LoadScene("LoseFP");
+      }
+
+    }
+}
+            
+
