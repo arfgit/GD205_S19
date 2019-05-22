867a13dd21d7fe27640f6c76d85ab5d6a0f3ec45
Anthony Feliz <47192556+arfgit@users.noreply.github.com>
2019-05-22 09:05:36 -0400

    update

---
+++ Apocalypse/Assets/Script/ScriptsFP/MyNavAgent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+public class MyNavAgent : MonoBehaviour
+{
+   
+    NavMeshAgent nav;
+    public Transform target;
+
+
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+   
+    void Update()
+    {
+       nav.SetDestination(target.transform.position);
+    }
+
+   /*  void OnTriggerStay(Collider other)
+    {
+        Debug.Log("your trigger hit... " + other.name);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            nav.destination = other.transform.position;
+        }
+    } */
+
+    
+
+}
